faa440c6bc467c67025ac0b1d38dc61596eeb378
kn4y4n <kn4y4nnn@gmail.com>
2023-05-12 15:45:44 +0530

    feat: prepare indexer

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+from dataclasses import dataclass
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+@dataclass
+class Paths:
+    root: Path = Path(__file__).parent
+    data: Path = root / "data"
+    book: Path = (
+        data
+        / "Marcus_Aurelius_Antoninus_-_His_Meditations_concerning_himselfe/index.html"
+    )
+
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+elastic_search_url = os.getenv("ELASTIC_SEARCH_URL")
